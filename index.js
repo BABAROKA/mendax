224c0ddc4dd6e516cea110d0d1411bde00ecf462
BABAROKA <ledi01001100@gmail.com>
2024-11-13 15:21:30 +0100

    changing how animations function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function checkClickOutside(event, button, input) {
     if (event.target !== button && event.target !== input) {
         document.removeEventListener('mousedown', event => {checkClickOutside(event, button, input)});
 
-        input.classList.remove('show');
+        input.style.animation = "shrink 0.2s linear forwards";
         button.style.display = "block";
     }
 }
@@ -11,18 +11,24 @@ function checkClickOutside(event, button, input) {
 function toggleSearch() {
     const button = document.getElementById("toggle-button");
     const input = document.getElementById("toggle-input");
+
     button.style.display = "none";
-    input.classList.add('show');
+    input.style.display = "block";
+    input.style.animation = "grow 0.2s linear";
 
     document.addEventListener('mousedown', event => {
         checkClickOutside(event, button, input);
     });
 }
 
+let isLike;
 function like(like) {
-    if (like.children[0].children[0].classList.contains("liked")) {
-        like.children[0].children[0].classList.remove("liked");
+    if (isLike) {
+        like.children[0].children[0].style.animation = "dislike 0.2s ease-out forwards";
+        isLike = false;
         return
     }
-    like.children[0].children[0].classList.add("liked");
+    
+    like.children[0].children[0].style.animation = "like 0.2s ease-out forwards";
+    isLike = true;
 }
