5e974735f0234b2fd55121bc5dc6a699c3f83f6a
BABAROKA <ledi01001100@gmail.com>
2024-11-13 18:38:58 +0100

    finished like animation and made different oer each post

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-
 function checkClickOutside(event, button, input) {
-    if (event.target !== button && event.target !== input) {
+    if (event.target != input) {
         document.removeEventListener('mousedown', event => {checkClickOutside(event, button, input)});
-
-        input.style.animation = "shrink 0.2s linear forwards";
+        
+        input.style.display = "none";
         button.style.display = "block";
     }
 }
 
 function toggleSearch() {
-    const button = document.getElementById("toggle-button");
-    const input = document.getElementById("toggle-input");
+    let button = document.getElementById("toggle-button");
+    let input = document.getElementById("toggle-input");
 
-    button.style.display = "none";
     input.style.display = "block";
+    button.style.display = "none";
     input.style.animation = "grow 0.2s linear";
 
     document.addEventListener('mousedown', event => {
         checkClickOutside(event, button, input);
     });
 }
-
-let isLike;
+let isLike = {};
 function like(like) {
-    if (isLike) {
+    if (isLike[like.id]) {
         like.children[0].children[0].style.animation = "dislike 0.2s ease-out forwards";
-        isLike = false;
+        isLike[like.id] = false;
         return
     }
     
     like.children[0].children[0].style.animation = "like 0.2s ease-out forwards";
-    isLike = true;
+    isLike[like.id] = true;
 }
